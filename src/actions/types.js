c99f71f6701252c764236ebb72f5a78c83a4a1d3
akshada <akshadasipsap@gmail.com>
2021-06-30 18:11:15 +0530

    Updates

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = 'CREATE_POST';
-export const UPDATE_POST = 'UPDATE_POST';
-export const LIKED_POST = 'LIKED_POST';
-export const DISLIKED_POST = 'DISLIKED-POST';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import postsReducer from './postsReducer';
-import likeDislikeReducer from './likeDislikeReducer'
-
-export default combineReducers({
-	posts : postsReducer,
-	likedDislikedPosts: likeDislikeReducer,
-})
